8b70e818b9d09b4697d20ea05e7f916929271b7f
William English tus22 <K00244462@student.tus.ie>
2023-12-05 13:33:39 +0000

    Player Movement and started animations

--- 2D_game_William_English/Assets/Scripts/PlayerContoller.cs
+++ 2D_game_William_English/Assets/Scripts/PlayerContoller.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+        if (Input.GetKeyDown("space"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 14f);
+        }
         
     }
 }
