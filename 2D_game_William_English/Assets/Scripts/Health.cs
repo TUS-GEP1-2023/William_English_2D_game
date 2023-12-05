7e3f814e7c89522f40e4b477f02ecf23ad898c53
William English tus22 <K00244462@student.tus.ie>
2023-12-05 18:19:52 +0000

    Health and Basic enemy

--- 2D_game_William_English/Assets/Scripts/Health.cs
+++ 2D_game_William_English/Assets/Scripts/Health.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
-    public int maxHealth = 3;
+    public int maxHealth = 5;
     public int currentHealth;
+    private Rigidbody2D rb;
+    private Animator anim;
     void Start()
     {
         currentHealth = maxHealth;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Enemies"))
+        { 
+            TakeDamage();
+            
+        }
+    }
+
+    void TakeDamage()
+    {
+        currentHealth--;
+        if (currentHealth <= 0) 
+        {
+                 rb.bodyType = RigidbodyType2D.Static;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
